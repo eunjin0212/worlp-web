e3f0719ca76aabe1e23416525890e5cf7338deb2
eunjin0212 <eunjin9639@gmail.com>
2024-01-10 22:49:42 +0900

    🎨 Change structure src > app

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,31 @@ import { useState } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/page.module.css'
 import appstore from '@/assets/appstore.svg'
-import SearchInput from '@/components/searchInput'
+import SearchInput from '../components/searchInput'
 import Chip from '@/components/chip'
+import api from '@/api/api'
+
+interface Feed {
+  id: string;
+  cheapest: {
+    vendorProductID: string;
+    vendor: string;
+    title: string;
+    thumbnail: string;
+    link: string;
+    price: {
+      listPrice: {
+        decimal: number,
+        fraction: number,
+        currency: string;
+      }
+    },
+  }
+}
+
+interface FeedList {
+  list: Feed[]
+}
 
 const categories = ['전체', '베스트', '할인율 높은순', '신상품']
 export default function Home() {
@@ -18,6 +41,14 @@ export default function Home() {
   const handleClick = (value: string) => {
     setCategory(value)
     console.log(value)
+  }
+
+  const fetch = async () => {
+    try {
+      const { data } = await api.get('feed/v1')
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
